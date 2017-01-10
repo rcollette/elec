bf6ed625ff4ecfecb2a8ddbe5a4f6c6c4da15238
Richard Collette <rcollette@yahoo.com>
2017-01-10 00:52:26 -0500

    add viewer

--- app/main.ts
+++ app/main.ts
@@ -12,7 +12,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, "index.html"),
+    pathname: path.join(__dirname, "renderer/menu/menu.html"),
     protocol: "file:",
     slashes: true
   }));
