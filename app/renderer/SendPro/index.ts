53bd0d09f79b19702300deb88d83a8a8d0187292
Richard Collette <richard.collette@pb.com>
2017-01-12 00:57:06 -0500

    Fix opening SendPro and launching webview debugger

--- app/renderer/SendPro/index.ts
+++ app/renderer/SendPro/index.ts
@@ -1,8 +1,47 @@
-"use strict";
-import {remote} from "electron";
+import * as request from 'request';
+import * as fs from 'fs';
+import * as path from 'path';
+import {config} from '../../modules/config';
 
-debugger;
-//Need to do this (or similar) because the debugger window ia associated with the main window and not the webview.
-let webView = <Electron.WebViewElement>document.querySelector("webview");
-let contents = webView.getWebContents();
-contents.on("did-finish-load", () => contents.openDevTools());
+window.onload = () => {
+  const webview = <Electron.WebViewElement>document.getElementById('aWebView');
+  const indicator = <HTMLDivElement>document.querySelector('.indicator');
+
+  const domReady = () => {
+    // Handler when the DOM is fully loaded
+    if (webview) {
+      try {
+        //TODO register a menu item shown only in devMode that does this.
+        webview.getWebContents().openDevTools();
+      } catch (e) {
+        alert(e.message);
+      }
+    }
+  };
+
+  const loadstart = () => {
+    //TODO implement something more graphical for loading indicator.
+    indicator.innerText = 'loading...';
+  };
+
+  const loadstop = () => {
+    indicator.innerText = '';
+    indicator.style.display = 'none';
+    webview.style.visibility = 'visible';
+    webview.openDevTools();
+  };
+
+  webview.addEventListener('did-start-loading', loadstart);
+  webview.addEventListener('did-stop-loading', loadstop);
+  //Not sure which opens the debugger earlier this or dom-ready
+  webview.addEventListener('did-finish-loading', domReady);
+
+  const contents = webview.getWebContents();
+  contents.session.on('will-download', (event, item) => {
+    event.preventDefault();
+    request(item.getURL(), (data) => {
+      fs.writeFileSync(path.join(__dirname, config.filePaths.virtual, item.getFilename()), data);
+    });
+  });
+  contents.on('did-finish-load', () => contents.openDevTools());
+};
--- app/renderer/menu/menu.ts
+++ app/renderer/menu/menu.ts
@@ -1,29 +1,31 @@
-import { remote } from "electron";
-import * as path from "path";
+import {remote} from 'electron';
+import * as path from 'path';
 
 const BrowserWindow = remote.BrowserWindow;
-
-function windowLink(linkId: string, filePath: string) {
-  const link = document.getElementById("viewerLink");
-  link.addEventListener("click", function (event: any) {
+function windowLink(linkId: string, filePath: string, callback?: (window: Electron.BrowserWindow) => void): void {
+  let link = document.getElementById(linkId);
+  link.addEventListener('click', function (event: any) {
     event.preventDefault();
     // protocol seem to be required on a MAC
-    const thePath = path.join("file:", __dirname, filePath);
-    let win = new BrowserWindow({ width: 800, height: 800 });
-    win.webContents.openDevTools();
-    win.on("close", function () {
+    const thePath = path.join('file:', __dirname, filePath);
+    let win = new BrowserWindow({width: 800, height: 800});
+    //win.webContents.openDevTools();
+    win.on('close', function () {
       win = null;
     });
     win.loadURL(thePath);
     win.show();
+    if (callback) {
+      callback(win);
+    }
   });
 }
 
 function registerListeners() {
-  windowLink("viewerLink", "../viewer/web/viewer.html");
-  windowLink("sendproLink", "../sendpro/index.html");
+  windowLink('viewerLink', '../viewer/web/viewer.html?file=https://www.cga.ct.gov/2017/TOB/h/pdf/2017HB-05200-R00-HB.pdf');
+  windowLink('sendproLink', '../sendpro/index.html');
 }
 
-document.addEventListener("DOMContentLoaded", function (event: any) {
+document.addEventListener('DOMContentLoaded', function (event: any) {
   registerListeners();
 });
