b8f265430e881946790804021cd0cef0c578f9d5
Richard Collette <rcollette@yahoo.com>
2017-01-11 21:46:29 -0500

    Open SendPro via a link handler

--- app/renderer/menu/menu.ts
+++ app/renderer/menu/menu.ts
@@ -1,16 +1,15 @@
-import {remote} from "electron";
+import { remote } from "electron";
 import * as path from "path";
 
 const BrowserWindow = remote.BrowserWindow;
 
-
-function registerListeners() {
-  const viewerLink = document.getElementById("viewerLink");
-  viewerLink.addEventListener("click", function (event: any) {
+function windowLink(linkId: string, filePath: string) {
+  const link = document.getElementById("viewerLink");
+  link.addEventListener("click", function (event: any) {
     event.preventDefault();
     // protocol seem to be required on a MAC
-    const thePath = path.join("file:", __dirname, "../viewer/web/viewer.html");
-    let win = new BrowserWindow({width: 800, height: 800});
+    const thePath = path.join("file:", __dirname, filePath);
+    let win = new BrowserWindow({ width: 800, height: 800 });
     win.webContents.openDevTools();
     win.on("close", function () {
       win = null;
@@ -18,6 +17,11 @@ function registerListeners() {
     win.loadURL(thePath);
     win.show();
   });
+}
+
+function registerListeners() {
+  windowLink("viewerLink", "../viewer/web/viewer.html");
+  windowLink("sendproLink", "../sendpro/index.html");
 }
 
 document.addEventListener("DOMContentLoaded", function (event: any) {
